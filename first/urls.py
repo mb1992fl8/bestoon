ebf606ed492849cacf3eb435517223f389191321
mb1992fl8 <mbessien@gmail.com>
2023-07-04 11:58:55 +0330

    Some corrections in income and expense binary files and urls file

--- first/urls.py
+++ first/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^submit/expense/?$', views.submit_expense, name='submit_expense'),
     url(r'^submit/income/?$', views.submit_income, name='submit_income'),
     url(r'^q/generalstat/?$', views.generalstat, name='generalstat'),
-    url(r'^accounts/register/$', views.register, name='register'),
+    url(r'^accounts/register/?$', views.register, name='register'),
     url(r'^$', views.index, name='index'),
 
 ]
