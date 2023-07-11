34c6696f74ed79093b05dbd1d605911182954138
mb1992fl8 <mbessien@gmail.com>
2023-07-12 01:21:34 +0330

    Separated utils. merging itmard master. but CAUTION! I do not know what are these

--- clients/ionic/bestoon/www/js/app.js
+++ clients/ionic/bestoon/www/js/app.js
@@ -104,10 +104,10 @@ function back_to_login_page($scope, $state) {
 
 
 var storage = window.localStorage;
+
 var token = storage.getItem('token');
 
 bestoonURL = storage.getItem('bestoonURL')
-console.log(bestoonURL)
 
 if (!bestoonURL) {
   var bestoonURL = 'http://localhost:8009/';
